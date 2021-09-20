02d96458f409ec57995baff61a146ca775f1a769
facu.ib <facundoemiliano.21@gmail.com>
2021-09-20 19:54:21 -0300

    Desafio clase 2 Terminado

---
+++ Desafio clase 2/index.js
@@ -0,0 +1,18 @@
+let usuario = prompt("Ingrese su usuario")
+let seguridad = prompt ("¿Cual es tu cumpleaños?")
+
+if (usuario == "juan" && seguridad == "25 de agosto"){
+    alert (`Acceso conseguido juan, felicidades`)
+}
+else if (usuario == "ramiro" && seguridad == "19 de diciembre"){
+    alert (`Acceso conseguiro Ramiro, felicidades`)
+}
+else if (usuario == "jorge" && seguridad == "5 de noviembre"){
+    alert (`Acceso conseguiro Jorge, felicidades`)
+}
+else{
+    alert (`Tomatela porque no sos de aca`)
+}
+
+
+

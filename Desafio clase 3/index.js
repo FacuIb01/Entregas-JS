2e1ca090daa659100fe1cdc7c46777de32c9ba77
facu.ib <facundoemiliano.21@gmail.com>
2021-09-27 17:10:33 -0300

    Desafio clase 3

---
+++ Desafio clase 3/index.js
@@ -0,0 +1,17 @@
+let numero = Number(prompt("Ingrese un numero"))
+
+
+for(i=0; i < numero; i++){
+    console.log("Hola!")
+}
+
+
+let frase = "ESC"
+
+while(i != frase){
+    i = prompt("Ingrese una frase")
+    console.log(i + " Esta es su frase")
+}
+
+
+

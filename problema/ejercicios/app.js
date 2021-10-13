70e2270559a28ac9f54bb8a706a2e9327f7e2893
facu.ib <facundoemiliano.21@gmail.com>
2021-10-13 20:16:25 -0300

    problema

---
+++ problema/ejercicios/app.js
@@ -0,0 +1,10 @@
+let texto = document.getElementById("texto")
+let boton = document.getElementsByTagName("button")
+
+
+boton.addEventListener ("click", (e) => {
+    e.preventDefault()
+
+    console.log(input.value)
+
+})  
